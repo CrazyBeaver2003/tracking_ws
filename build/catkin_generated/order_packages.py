79b7d4a55721f79e62ef0bbe79052c5f009991ca
CrazyBeaver2003 <timagadenov1@gmail.com>
2025-04-19 19:09:48 +0300

    Created ROS workspace

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/timoha/Coding/tracking_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/timoha/aerobot_ws/devel;/opt/ros/noetic'.split(';') if '/home/timoha/aerobot_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/timoha/Coding/tracking_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/timoha/aerobot_ws/devel/lib:/opt/ros/noetic/lib:/tmp/.mount_CursorhkoGGf/usr/lib/:/tmp/.mount_CursorhkoGGf/usr/lib32/:/tmp/.mount_CursorhkoGGf/usr/lib64/:/tmp/.mount_CursorhkoGGf/lib/:/tmp/.mount_CursorhkoGGf/lib/i386-linux-gnu/:/tmp/.mount_CursorhkoGGf/lib/x86_64-linux-gnu/:/tmp/.mount_CursorhkoGGf/lib/aarch64-linux-gnu/:/tmp/.mount_CursorhkoGGf/lib32/:/tmp/.mount_CursorhkoGGf/lib64/:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:/home/timoha/ardupilot_gazebo/build:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:/home/timoha/ardupilot_gazebo/build:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:/home/timoha/ardupilot_gazebo/build:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins'
+export PWD='/home/timoha/Coding/tracking_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/timoha/Coding/tracking_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/timoha/Coding/tracking_ws/src:$ROS_PACKAGE_PATH"
