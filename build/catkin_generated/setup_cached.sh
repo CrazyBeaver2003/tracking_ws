9cfce67b4238215aa9a3df034a96258f36d4ae12
CrazyBeaver2003 <timagadenov1@gmail.com>
2025-04-25 18:42:59 +0300

    Pushed raw_project, where user can select roi for detection

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/timoha/Coding/tracking_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/timoha/Coding/tracking_ws/devel/lib:/opt/ros/noetic/lib:/tmp/.mount_CursorajiKDM/usr/lib/:/tmp/.mount_CursorajiKDM/usr/lib32/:/tmp/.mount_CursorajiKDM/usr/lib64/:/tmp/.mount_CursorajiKDM/lib/:/tmp/.mount_CursorajiKDM/lib/i386-linux-gnu/:/tmp/.mount_CursorajiKDM/lib/x86_64-linux-gnu/:/tmp/.mount_CursorajiKDM/lib/aarch64-linux-gnu/:/tmp/.mount_CursorajiKDM/lib32/:/tmp/.mount_CursorajiKDM/lib64/:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:/home/timoha/ardupilot_gazebo/build:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:/home/timoha/ardupilot_gazebo/build:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:/home/timoha/ardupilot_gazebo/build:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins'
+export PKG_CONFIG_PATH="/home/timoha/Coding/tracking_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/timoha/Coding/tracking_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/timoha/Coding/tracking_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/timoha/Coding/tracking_ws/src:$ROS_PACKAGE_PATH"
